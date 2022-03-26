07c9d8e552a90c8ad20c7a18fd1aafc0e1830bd0
Maituo Yu <37908868+maituoy@users.noreply.github.com>
2022-03-26 14:45:08 -0400

    Update ViT.py

--- Models/ViT.py
+++ Models/ViT.py
@@ -33,138 +33,138 @@ class Attention(nn.Module):
 
 class MLPBlock(nn.Sequential):
 
-	def __init__(self, in_dim, mlp_dim, dropout=0):
-		super().__init__()
-		
-		self.linear_1 = nn.Linear(in_dim, mlp_dim)
-		self.gelu = nn.GELU()
-		self.dropout_1 = nn.Dropout(dropout)
-		self.linear_2 = nn.Linear(mlp_dim, in_dim)
-		self.dropout_2 = nn.Dropout(dropout)
+    def __init__(self, in_dim, mlp_dim, dropout=0):
+        super().__init__()
+        
+        self.linear_1 = nn.Linear(in_dim, mlp_dim)
+        self.gelu = nn.GELU()
+        self.dropout_1 = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(mlp_dim, in_dim)
+        self.dropout_2 = nn.Dropout(dropout)
 
 class EncoderBlock(nn.Module):
 
-	def __init__(self, num_heads, hidden_dim, mlp_dim, dropout, attention_dropout):
-		super().__init__()
-		
-		self.num_heads = num_heads
+    def __init__(self, num_heads, hidden_dim, mlp_dim, dropout, attention_dropout):
+        super().__init__()
+        
+        self.num_heads = num_heads
 
-		# Components of the first half block
-		self.ln_1 = nn.LayerNorm(hidden_dim)
-		self.self_attention = Attention(hidden_dim, 
-										num_heads, 
-										attn_drop=attention_dropout
-										)
-		self.dropout = nn.Dropout(dropout)
+        # Components of the first half block
+        self.ln_1 = nn.LayerNorm(hidden_dim)
+        self.self_attention = Attention(hidden_dim, 
+                                        num_heads, 
+                                        attn_drop=attention_dropout
+                                        )
+        self.dropout = nn.Dropout(dropout)
 
-		# Components of the second half block
-		self.ln_2 = nn.LayerNorm(hidden_dim)
-		self.mlp = MLPBlock(hidden_dim, mlp_dim, dropout)
+        # Components of the second half block
+        self.ln_2 = nn.LayerNorm(hidden_dim)
+        self.mlp = MLPBlock(hidden_dim, mlp_dim, dropout)
 
-	def forward(self, x):
+    def forward(self, x):
 
-		input_x = x.clone()
-		x = self.ln_1(x)
-		x, _ = self.self_attention(query=x, key=x, value=x, need_weights=False)
-		x = self.dropout(x)
-		x += input_x
+        input_x = x.clone()
+        x = self.ln_1(x)
+        x, _ = self.self_attention(query=x, key=x, value=x, need_weights=False)
+        x = self.dropout(x)
+        x += input_x
 
-		y = self.ln_2(x)
-		y = self.mlp(y)
-		y += x
+        y = self.ln_2(x)
+        y = self.mlp(y)
+        y += x
 
-		return y
+        return y
 
 class Encoder(nn.Module):
 
-	def __init__(self, num_layers, num_heads, hidden_dim, mlp_dim, dropout, attention_dropout):
-		super().__init__()
+    def __init__(self, num_layers, num_heads, hidden_dim, mlp_dim, dropout, attention_dropout):
+        super().__init__()
 
-		self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(dropout)
 
-		layers = []
+        layers = []
 
-		for i in range(num_layers):
-			layers.append(EncoderBlock(num_heads, hidden_dim, mlp_dim, dropout, attention_dropout))
+        for i in range(num_layers):
+            layers.append(EncoderBlock(num_heads, hidden_dim, mlp_dim, dropout, attention_dropout))
 
-		self.layers = nn.Sequential(*layers)
-		self.ln = nn.LayerNorm(hidden_dim)
+        self.layers = nn.Sequential(*layers)
+        self.ln = nn.LayerNorm(hidden_dim)
 
-	def forward(self, x):
+    def forward(self, x):
 
-		x = self.dropout(x)
-		x = self.layers(x)
-		x = self.ln(x)
+        x = self.dropout(x)
+        x = self.layers(x)
+        x = self.ln(x)
 
-		return x
-		
+        return x
+        
 
 class ViT(nn.Module):
 
-	def __init__(
-		self, 
-		img_size, 
-		patch_size, 
-		num_layers, 
-		num_heads, 
-		hidden_dim, 
-		mlp_dim, 
-		dropout=0., 
-		attention_dropout=0., 
-		num_classes=1000
-		):
-		super().__init__()
+    def __init__(
+        self, 
+        img_size, 
+        patch_size, 
+        num_layers, 
+        num_heads, 
+        hidden_dim, 
+        mlp_dim, 
+        dropout=0., 
+        attention_dropout=0., 
+        num_classes=1000
+        ):
+        super().__init__()
 
-		self.patch_size = patch_size
-		self.hidden_dim = hidden_dim
+        self.patch_size = patch_size
+        self.hidden_dim = hidden_dim
 
-		self.embedding = nn.Conv2d(3, hidden_dim, patch_size, patch_size)
+        self.embedding = nn.Conv2d(3, hidden_dim, patch_size, patch_size)
 
-		seq_length = (img_size // patch_size) ** 2
+        seq_length = (img_size // patch_size) ** 2
 
-		# Class token
-		self.cls_token = nn.Parameter(torch.zeros(1,1,hidden_dim))
-		seq_length += 1
+        # Class token
+        self.cls_token = nn.Parameter(torch.zeros(1,1,hidden_dim))
+        seq_length += 1
 
-		# Position embedding
-		self.pos_embedding = nn.Parameter(torch.empty(1, seq_length, hidden_dim).normal_(std=0.02))
-		
+        # Position embedding
+        self.pos_embedding = nn.Parameter(torch.empty(1, seq_length, hidden_dim).normal_(std=0.02))
+        
 
-		self.encoder = Encoder(num_layers, num_heads, hidden_dim, mlp_dim, dropout, attention_dropout)
+        self.encoder = Encoder(num_layers, num_heads, hidden_dim, mlp_dim, dropout, attention_dropout)
 
-		self.head = nn.Linear(hidden_dim, num_classes)
+        self.head = nn.Linear(hidden_dim, num_classes)
 
-		self.seq_length = seq_length
+        self.seq_length = seq_length
 
-	def forward(self, x):
+    def forward(self, x):
 
-		n, c, h, w = x.shape
-		p = self.patch_size
+        n, c, h, w = x.shape
+        p = self.patch_size
 
-		n_h = h // p
-		n_w = w // p
+        n_h = h // p
+        n_w = w // p
 
-		# Patching and flatting the image based on patch size
-		x = self.embedding(x)
-		x = x.reshape(n, self.hidden_dim, n_h*n_w)
-		x = x.permute(0, 2, 1)
+        # Patching and flatting the image based on patch size
+        x = self.embedding(x)
+        x = x.reshape(n, self.hidden_dim, n_h*n_w)
+        x = x.permute(0, 2, 1)
 
-		# Add a class token to each data
-		batch_cls_token = self.cls_token.expand(n, -1, -1)
-		x = torch.cat([batch_cls_token, x], dim=1)
+        # Add a class token to each data
+        batch_cls_token = self.cls_token.expand(n, -1, -1)
+        x = torch.cat([batch_cls_token, x], dim=1)
 
-		# Add position embedding
-		x += self.pos_embedding
+        # Add position embedding
+        x += self.pos_embedding
 
-		# Feed data into the encoder
-		x = self.encoder(x)
+        # Feed data into the encoder
+        x = self.encoder(x)
 
-		# Only the first value is used for classification
-		x = x[:, 0]
+        # Only the first value is used for classification
+        x = x[:, 0]
 
-		# Send x to the classificaiton layer
-		x = self.head(x)
+        # Send x to the classificaiton layer
+        x = self.head(x)
 
-		return x
+        return x
 
-		
+        
