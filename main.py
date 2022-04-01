6bf118d10796ad8749da05a8027a6faca0552363
Maituo Yu <37908868+maituoy@users.noreply.github.com>
2022-04-01 12:48:34 -0400

    Update for ddp

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from Models.ResNet import *
 from Dataset import *
 from prepare import *
+from utils import *
 
 import argparse
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 def parse_argument():
 
     parser = argparse.ArgumentParser(description='params')
-    parser.add_argument('-epochs', dest='epochs', type=int, default=50)
-    parser.add_argument('-bs', dest='bs', type=int, default=64)
+    parser.add_argument('-epochs', dest='epochs', type=int, default=100)
+    parser.add_argument('-bs', dest='bs', type=int, default=256)
     parser.add_argument('-lr', dest='lr', type=float, default=0.03)
     parser.add_argument('-wd', dest='wd', type=float, default=1e-3)
     parser.add_argument('-img_size', dest='img_size', type=int, default=256)
     parser.add_argument('-input_size', dest='input_size', type=int, default=224)
     parser.add_argument('-dataset', dest='dataset', type=str, default='cub')
+    parser.add_argument('-ngpu_used', dest='ngpu_used', type=int, default=1)
 
     return parser.parse_args()
 
@@ -28,6 +31,21 @@ def main():
     img_size = args.img_size
     input_size = args.input_size
     dataset = args.dataset
+    ngpu_used = args.ngpu_used
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    ddp = 0
+    if ngpu_used > 1:
+        local_rank = int(os.environ["LOCAL_RANK"])
+        rank = int(os.environ["RANK"])
+        world_size = int(os.environ['WORLD_SIZE'])
+        n_gpus = torch.cuda.device_count()
+
+        assert n_gpus >= ngpu_used, "GPU is not enough"
+
+        torch.cuda.set_device(local_rank)
+        torch.distributed.init_process_group(backend='nccl', init_method='env://', world_size=world_size, rank=rank)
+        torch.distributed.barrier()
+        ddp += 1
 
     train_transforms = transforms.Compose([transforms.Resize((img_size, img_size), Image.BILINEAR),
                                        transforms.RandomCrop((input_size, input_size)),
@@ -41,35 +59,46 @@ def main():
                                            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
 
     if dataset == 'cub':
-        train_dataset = CUB2011(root='./', transform=train_transforms, train=True, extract=True)
-        test_dataset = CUB2011(root='./', transform=test_transforms, train=False, extract=True)
+        train_dataset = CUB2011(root='/media/Bootes/dl_fgvc/', transform=train_transforms, train=True, extract=False)
+        test_dataset = CUB2011(root='/media/Bootes/dl_fgvc/', transform=test_transforms, train=False, extract=False)
 
     elif dataset == 'dog':
-        train_dataset = StandfordDog(root='./', transform=train_transforms, train=True, extract=True)
-        test_dataset = StandfordDog(root='./', transform=test_transforms, train=False, extract=True)
+        train_dataset = StandfordDog(root='/media/Bootes/dl_fgvc/', transform=train_transforms, train=True, extract=False)
+        test_dataset = StandfordDog(root='/media/Bootes/dl_fgvc/', transform=test_transforms, train=False, extract=False)
 
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, 
-                                                  shuffle=True, num_workers=1)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, 
-                                              shuffle=False, num_workers=1)
+    if ddp:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+        test_sampler = torch.utils.data.distributed.DistributedSampler(test_dataset)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size//ngpu_used, 
+                                                    shuffle=False, num_workers=8, sampler=train_sampler)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size//ngpu_used, 
+                                                shuffle=False, num_workers=8, sampler=test_sampler)
+    else:
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
 
     len_train = len(train_loader)
 
     model = resnet50(pretrained=True)
     model.fc = nn.Linear(2048, 200)
+    model = model.cuda()
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    criterion, optimizer, scheduler, scaler = set_up(model, device, lr, weight_decay, len_train, epochs, warmup_epochs=5)
 
-    criterion, optimizer, scheduler, scaler = set_up(model, device, lr, weight_decay, len_train, epochs)
+    if ddp:
+        model = DDP(model, device_ids=[local_rank])
 
     for epoch in range(epochs):
-        train_accuracy, train_loss, learning_rate = train(model, device, batch_size, train_loader, optimizer, criterion, scheduler, scaler)
-        print("Epoch {}/{}: Train Acc {:.04f}%, Train Loss {:.04f}, Learning Rate {:.04f}".format(epoch + 1, epochs, train_accuracy, train_loss, learning_rate))
+        if ddp:
+            train_one_epoch(epoch, model, train_loader, optimizer, criterion, scheduler, scaler)
+            validate(model, test_loader, criterion)
+        else:
+            train_accuracy, train_loss, learning_rate = train(model, device, batch_size, train_loader, optimizer, criterion, scheduler, scaler)
+            print("Epoch {}/{}: Train Acc {:.04f}%, Train Loss {:.04f}, Learning Rate {:.04f}".format(epoch + 1, epochs, train_accuracy, train_loss, learning_rate))
 
-        if not (epoch + 1) % 10 and epoch > 0:
-            test_accuracy = evaluate(model, device, batch_size, test_loader, test_dataset)
-            print("Test: {:.04f}%".format(test_accuracy))
+            if not (epoch + 1) % 10 and epoch > 0:
+                test_accuracy = evaluate(model, device, batch_size, test_loader, test_dataset)
+                print("Test: {:.04f}%".format(test_accuracy))
 
 
 if __name__ == "__main__":
--- prepare.py
+++ prepare.py
@@ -1,8 +1,13 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.distributed as dist
 
 from tqdm import tqdm
+from warmup_scheduler import GradualWarmupScheduler
+from timm.utils import AverageMeter, accuracy
+from utils import reduce_tensor
+
 
 def get_parameter_num(model):
     num_trainable_parameters = 0
@@ -11,7 +16,7 @@ def get_parameter_num(model):
     
     return num_trainable_parameters
 
-def set_up(model, device, lr, weight_decay, len_train, epochs):
+def set_up(model, device, lr, weight_decay, len_train, epochs, warmup_epochs=None):
 
     model = model
     model.to(device)
@@ -19,6 +24,8 @@ def set_up(model, device, lr, weight_decay, len_train, epochs):
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
     scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=(len_train * epochs))
+    if warmup_epochs:
+        scheduler = GradualWarmupScheduler(optimizer, multiplier=1, total_epoch=warmup_epochs*len_train, after_scheduler=scheduler)
 
     scaler = torch.cuda.amp.GradScaler()
 
@@ -95,3 +102,60 @@ def evaluate(model, device, batch_size, test_loader, test_dataset):
 
     return test_accuracy
 
+def train_one_epoch(epoch, model, train_loader, optimizer, criterion, scheduler, scaler):
+    model.train()
+    optimizer.zero_grad()
+
+    num_steps = len(train_loader)
+    batch_bar = tqdm(total=num_steps, dynamic_ncols=True, leave=False, position=0, desc='Train') 
+
+    losses_m = AverageMeter()
+
+    for idx, (samples, targets) in enumerate(train_loader):
+        samples, targets = samples.cuda(non_blocking=True), targets.cuda(non_blocking=True)
+
+        with torch.cuda.amp.autocast():  
+            outputs = model(samples)
+            loss = criterion(outputs, targets)
+
+        loss = reduce_tensor(loss)
+        losses_m.update(loss.item(), num_steps)
+        batch_bar.set_postfix(
+            loss="{:.04f}".format(losses_m.avg),
+            lr="{:.04f}".format(float(optimizer.param_groups[0]['lr'])))
+
+        scaler.scale(loss).backward()
+        scaler.step(optimizer) 
+        scaler.update()
+        scheduler.step() 
+        batch_bar.update() 
+
+        torch.cuda.synchronize()
+        torch.cuda.empty_cache()
+
+    batch_bar.close()
+    if dist.get_rank() == 0:
+        print("Epoch {}: Train Loss {:.04f}, Learning Rate {:.04f}".format(epoch + 1, losses_m.avg, float(optimizer.param_groups[0]['lr'])))
+
+def validate(model, test_loader, criterion):
+
+    losses_m = AverageMeter()
+    acc_m = AverageMeter()
+    num_steps = len(test_loader)
+    for idx, (samples, targets) in enumerate(test_loader):
+
+        samples, targets = samples.cuda(non_blocking=True), targets.cuda(non_blocking=True)
+
+        output = model(samples)
+
+        loss = criterion(output, targets)
+        acc = accuracy(output, targets, topk=(1,))[0]
+
+        loss = reduce_tensor(loss)
+        acc = reduce_tensor(acc)
+
+        losses_m.update(loss.item(), num_steps)
+        acc_m.update(acc.item(), num_steps)
+        torch.cuda.empty_cache()
+    if dist.get_rank() == 0:
+        print("Test Loss: {:.04f}, Test Acc: {:.04f}%".format(losses_m.avg, acc_m.avg))
