d7da763747ea6e52f00a8565587958e5f93e1479
Maituo Yu <37908868+maituoy@users.noreply.github.com>
2022-04-12 23:36:30 -0400

    update modified resnet

--- Models/ResNet_comp1_micro.py
+++ Models/ResNet_comp1_micro.py
@@ -17,69 +17,9 @@ import torch.nn as  nn
 import torch.nn.functional as F
 from timm.models.layers import DropPath
 from torch.hub import load_state_dict_from_url
+import sys
 
-# layer norm copied from convnext.py
-class LayerNorm(nn.Module):
-    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first. 
-    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
-    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
-    with shape (batch_size, channels, height, width).
-    """
-    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(normalized_shape))
-        self.bias = nn.Parameter(torch.zeros(normalized_shape))
-        self.eps = eps
-        self.data_format = data_format
-        if self.data_format not in ["channels_last", "channels_first"]:
-            raise NotImplementedError 
-        self.normalized_shape = (normalized_shape, )
-    
-    def forward(self, x):
-        if self.data_format == "channels_last":
-            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
-        elif self.data_format == "channels_first":
-            u = x.mean(1, keepdim=True)
-            s = (x - u).pow(2).mean(1, keepdim=True)
-            x = (x - u) / torch.sqrt(s + self.eps)
-            x = self.weight[:, None, None] * x + self.bias[:, None, None]
-            return x
-
-# class Block(nn.Module):
-#     def __init__(self, in_channels, out_channels, ownsample=None, drop_path=0, stride=1):
-#         super().__init__()
-
-#         self.expansion = 
-#         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, stride=stride, bias=False)
-#         self.bn1 = nn.BatchNorm2d(out_channels)
-#         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=1, bias=False)
-#         self.bn2 = nn.BatchNorm2d(out_channels)
-
-#         self.gelu = nn.GELU()
-#         self.downsample = downsample
-        
-#         self.drop_path = DropPath(drop_path) if drop_path > 0 else nn.Identity()
-
-    
-#     def forward(self, x):
-
-#         identity = x.clone()
-
-#         x = self.conv1(x)
-#         x = self.bn1(x)
-#         x = self.gelu(x)
-
-#         x = self.conv2(x)
-#         x = self.bn2(x)
-
-#         if self.downsample is not None:
-#             identity = self.downsample(identity)
-        
-#         x = self.drop_path(x) + identity
-#         x = self.gelu(x)
-
-#         return x
-
+sys.path.append('../')
 
 class Bottleneck(nn.Module):
     def __init__(self, in_channels, out_channels, downsample=None, drop_path=0, stride=1):
