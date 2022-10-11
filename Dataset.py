4cdf9a85c2bd67d73ea80cf322015b4524385676
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-11 05:53:21 +0000

    Adding tarfile member sanitization to extractall()

--- Dataset.py
+++ Dataset.py
@@ -44,7 +44,26 @@ class CUB2011(Dataset):
 
         else:
             with tarfile.open(os.path.join(self.root, 'CUB_200_2011.tar'), 'r') as tar:
-                tar.extractall(path=self.root)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=self.root)
   
     def __len__(self):
         return len(self.dataset)
@@ -109,10 +128,48 @@ class StandfordDog(Dataset):
 
         else:
             with tarfile.open(os.path.join(self.root, 'images.tar'), 'r') as tar:
-                tar.extractall(path=os.path.join(self.root, self.filename))
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=os.path.join(self.root,self.filename))
 
             with tarfile.open(os.path.join(self.root, 'lists.tar'), 'r') as tar:
-                tar.extractall(path=os.path.join(self.root, self.filename))
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=os.path.join(self.root,self.filename))
 
     def __len__(self):
         return len(self.dataset)
